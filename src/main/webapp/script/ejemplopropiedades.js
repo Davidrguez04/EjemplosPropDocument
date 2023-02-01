1d8472c8db9ec8bd135febedc2e45580c1301f0f
Davidrguez04 <122359982+Davidrguez04@users.noreply.github.com>
2023-02-01 09:31:27 +0100

    new

--- src/main/webapp/script/ejemplopropiedades.js
+++ src/main/webapp/script/ejemplopropiedades.js
@@ -2,7 +2,10 @@
  * 
  */
 function cambiaColor(colorin){
-
+//funcion cambia color
  document.bgColor = colorin 
  
+}
+function modificacionUltima() {
+		document.write("La última modificación del documento es: " + document.lastModified);
 }
